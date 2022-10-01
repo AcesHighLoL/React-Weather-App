e015bc12d8b207d4f18f968ff5fc4c33e390d5a4
George Guardia <georgeguardia01@gmail.com>
2022-10-01 13:00:01 -0400

    Search bar and API search

---
+++ src/Api.js
@@ -0,0 +1,12 @@
+
+
+
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '3a370ade8bmshdd74592c0a8eaaep135a37jsnfb484d5b521e',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
